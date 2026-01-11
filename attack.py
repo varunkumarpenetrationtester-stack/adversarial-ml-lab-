f6ad4b858224775cd58583be0070db2533dd7b6a
varunkumarpenetrationtester-stack <varunkumarpenetrationtester@gmail.com>
2026-01-11 19:00:56 +0530

    Update and rename model/attack.py to attack.py

--- attack.py
+++ attack.py
@@ -1,9 +1,7 @@
-import numpy as np
-
 def poison_data(df, poison_rate=0.3):
     poisoned = df.copy()
     n = int(len(df) * poison_rate)
-    idx = np.random.choice(df.index, n, replace=False)
+    idx = df.sample(n).index
     poisoned.loc[idx, "label"] = 1 - poisoned.loc[idx, "label"]
     return poisoned
 
